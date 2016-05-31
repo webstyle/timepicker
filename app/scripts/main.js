ccaaa38ce7c40d0d502dc60a615dad7d610bc7dc
Farrukh <webstyle@protonmail.com>
2016-05-31 18:28:45 +0500

    time formating

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,83 +1,207 @@
-var worklyPicker = function (element, duration) {
-
-  var dates = {};
-  dates.v1 = [];
-  dates.v2 = [];
-  dates.v3 = [];
-  var start = null;
-  var end = null;
-
-  getDates();
+(function ($) {
 
   /**
-   * v3 with jquery ui autocomplete
+   * Save qilganda start va end timelarni validatsiya qilib qaytaradi.
+   * @returns {{start: *, end: *}}
    */
-  $(element).autocomplete({
-    source: dates.v3,
-    select: function (event, ui) {
-      var value = ui.item.value + " - ";
-      $(element).val(value);
-      event.preventDefault();
-      var selectedObj = ui.item;
-      $(element).val(value);
+  $.fn.timesPickerSave = function () {
+    var formValue = $(this).val().split(" - ");
+    var first = formValue[0];
+    var second = formValue[1];
+    var start, end;
+
+    /**
+     * Timeni regEx yordamida to'g'rilash funksiyasi
+     * @param str
+     * @returns {string|XML}
+     */
+    function time(str) {
+
+      if (!/:/.test(str)) {
+        str += ':00';
+      }
+
+      var strArray = str.split("");
+      var regexStart, regexEnd;
+
+      regexStart = (strArray.length > 2) ? /^\d{1}:/ : /^\d{2}:/;
+      regexEnd = /:\d{1}$/;
+
+      return str
+        .replace(regexStart, '0$&')
+        .replace(regexEnd, '$&0');
     }
-  });
 
-  $(element).on('input', function (value) {
 
-    var result = value.currentTarget.value.split(' - ');
+    console.log(formValue.length);
 
-    if (result.length > 1) {
+    if (formValue.length > 1) {
 
-      start = result[0];
-      end = result[1];
+      if (parseInt(formValue[0]) < 24 || parseInt(formValue[1])) {
+        start = time(formValue[0]);
+        end = time(formValue[1]);
 
-      if (end) {
-        $(element).autocomplete("search", end);
+        var result = {
+          start: start,
+          end: end
+        };
 
-        $(element).autocomplete({
-          select: function (event, ui) {
+        $(this).val(result.start + " - " + result.end);
+        return result;
 
-            var formValue = $(this).val().split(" ");
-            var value;
-
-            if (formValue[1]) {
-              value = formValue[0] + " - " + ui.item.value;
-            } else {
-              value = ui.item.value + " - ";
-            }
-
-            event.preventDefault();
-            var selectedObj = ui.item;
-            $(element).val(value);
-
-          }
-        });
+      } else {
+        console.error('errorku format xato berdiz');
       }
     }
 
-  });
+
+  };
 
 
   /**
-   * get hours of the day
+   * Times Pickerga values qo'shish uchun
+   * @param time
    */
-  function getDates() {
-    var startDate = moment();
-    var endDate = moment();
-    startDate.startOf('Day');
-    endDate.endOf('Day');
-    var end = new Date(endDate._d);
-    for (var current = moment(startDate._d); current < end; current.add(duration, 'Minutes')) {
-      dates.v3.push(current.format("HH:mm"));
+  $.fn.timesPickerSetValues = function (time) {
+    var result = time.start + " - " + time.end;
+    $(this).val(result);
+  };
+
+  /**
+   * Times Pickerni ko'rsatish va autocomplete
+   */
+  $.fn.timesPicker = function (options) {
+
+    var dates = {};
+    dates = [];
+    var start = null;
+    var end = null;
+
+    var settings = $.extend({
+      'duration': 15,
+      'format': '24h'
+    }, options);
+
+    var format;
+
+    format = (settings.format == '24h') ? "HH:mm" : "h:mm a";
+
+    var element = this;
+    var duration = settings.duration;
+
+    getDates();
+
+
+    function split(val) {
+      return val.split(/,\s*/);
     }
-  }
-}
+
+    function extractLast(term) {
+      return split(term).pop();
+    }
+
+    /**
+     * v3 with jquery ui autocomplete
+     */
+    $(element).bind("keydown", function (event) {
+      if (event.keyCode === $.ui.keyCode.TAB &&
+        $(this).autocomplete("instance").menu.active) {
+        event.preventDefault();
+      }
+    }).autocomplete({
+      focus: function () {
+        // prevent value inserted on focus
+        return false;
+      },
+      source: function (request, response) {
+        // delegate back to autocomplete, but extract the last term
+        response($.ui.autocomplete.filter(
+          dates, extractLast(request.term)));
+      },
+      select: function (event, ui) {
+        var value = ui.item.value + " - ";
+        $(element).val(value);
+        event.preventDefault();
+        var selectedObj = ui.item;
+        $(element).val(value);
+      }
+    });
 
 
-worklyPicker("#ui", 15);
-console.log('Hi1');
+    $(element).on('input', function (value) {
+
+      var result = value.currentTarget.value.split(' - ');
+
+      if (result.length > 1) {
+
+        start = result[0];
+        end = result[1];
+
+        if (end) {
+          $(element).autocomplete("search", end);
+
+          $(element).bind("keydown", function (event) {
+            if (event.keyCode === $.ui.keyCode.TAB &&
+              $(this).autocomplete("instance").menu.active) {
+              event.preventDefault();
+            }
+          }).autocomplete({
+
+            focus: function () {
+              // prevent value inserted on focus
+              return false;
+            },
+
+            select: function (event, ui) {
+
+              var formValue = $(this).val().split(" - ");
+              var value;
+
+              if (formValue[1]) {
+                value = formValue[0] + " - " + ui.item.value;
+              } else {
+                value = ui.item.value + " - ";
+              }
+
+              event.preventDefault();
+              var selectedObj = ui.item;
+              $(element).val(value);
+            }
+
+          });
+        }
+      }
+
+    });
+
+    /*
+     * get hours of the day
+     */
+    function getDates() {
+      var startDate = moment();
+      var endDate = moment();
+      startDate.startOf('Day');
+      endDate.endOf('Day');
+      var end = new Date(endDate._d);
+      for (var current = moment(startDate._d); current < end; current.add(duration, 'Minutes')) {
+        dates.push(current.format(format));
+      }
+    }
+
+  };
+
+})(jQuery);
+
+
+$("ui").timesPickerSetValues({start: '10:30', end: '20:00'});
+
+$("#ui").timesPicker({
+  duration: 20,
+  format: '24h'
+});
 
 $(".save").click(function () {
-  console.log($("#ui").val());
+  console.log($("#ui").timesPickerSave());
 });
+
+
